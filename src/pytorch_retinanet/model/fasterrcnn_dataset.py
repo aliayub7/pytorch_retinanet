0f7cdc050b7eaadcdfb9df260fb278f8e7c70c3f
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-02-11 03:31:02 -0800

    Data dataset wrapper compatible with Faster R-CNN
    Wraps the Retinanet dataset, transforming it to a COCO-compatible
    format.

---
+++ src/pytorch_retinanet/model/fasterrcnn_dataset.py
@@ -0,0 +1,33 @@
+import torch
+
+from pytorch_retinanet.model import retinanet_dataset
+
+class ListDataset(retinanet_dataset.ListDataset):
+    def __getitem__(self, idx):
+        """Return a COCO-formated bounding box target"""
+
+        img, boxes, labels = super().__getitem__(idx)
+
+        num_objs = len(boxes)
+        boxes = boxes.float()
+        image_id = torch.tensor([idx])
+        if num_objs > 0:
+            area = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        else:
+            area = torch.tensor([])
+
+        # Suppose all instances are not crowd
+        iscrowd = torch.zeros((num_objs,), dtype=torch.int64)
+
+        target = {
+            'boxes': boxes,
+            'labels': labels,
+            'image_id': image_id,
+            'area': area,
+            'iscrowd': iscrowd
+        }
+
+        return img, target
+
+    def collate_fn(self, batch):
+        return tuple(zip(*batch))
