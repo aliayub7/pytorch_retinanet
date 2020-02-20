d77f518bffd8407db4e89d4c162fbc8a9ceca1e0
Jaclyn Brockschmidt <jcbrockschmidt@gmail.com>
2020-02-20 01:50:52 -0800

    Fix indentation

--- src/pytorch_retinanet/utils/pt_utils.py
+++ src/pytorch_retinanet/utils/pt_utils.py
@@ -181,10 +181,10 @@ def box_nms(bboxes, scores, threshold=0.5, mode='union'):
 
     keep = list()
     while order.numel() > 0:
-        if order.dim() == 0:	
-		i = order.item()
-	else:
-        	i = order[0]
+        if order.dim() == 0:
+            i = order.item()
+        else:
+            i = order[0]
         keep.append(i)
 
         if order.numel() == 1:
